87b00d922b6811e94bd72cd8cddd4eecbc9c263e
Matteo Altamore <maltamore@deloitte.it>
2022-09-15 22:21:11 +0200

    build: add lint setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+
+module.exports = {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "plugin:react/jsx-runtime",
+    "plugin:import/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-console": ["error", { allow: ["info"] }],
+  },
+};
